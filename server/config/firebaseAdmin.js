7ea7f3cfc3e09041a205dd4791d1f9c9db55544f
Ansh Aaditya Singh <anshaadityasingh@gmail.com>
2026-07-12 22:17:46 +0530

    Import cert directly from firebase-admin/app

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -1,9 +1,9 @@
 const admin = require('firebase-admin');
-const { getApps } = require('firebase-admin/app');
+const { initializeApp, getApps, cert } = require('firebase-admin/app');
 
 if (getApps().length === 0) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
+  initializeApp({
+    credential: cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       privateKey: process.env.FIREBASE_PRIVATE_KEY
         ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
